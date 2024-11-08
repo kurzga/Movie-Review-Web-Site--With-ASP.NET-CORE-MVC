71ff9a02e091303b181e3139681a5111844ca02a
kuzey <umutkuzey15@gmail.com>
2024-11-08 14:05:34 +0300

    Movie Review Web Site

---
+++ FilmSitesi/Controllers/GenreController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmSitesi.Controllers
+{
+    public class GenreController : Controller
+    {
+        // GET: Genre
+        public ActionResult Genres()
+        {
+            return View();
+        }
+        public ActionResult Horror()
+        {
+            return View();
+        }
+        public ActionResult Comedy()
+        {
+            return View();
+        }
+    }
+}
---
+++ FilmSitesi/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmSitesi.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult News()
+        {
+            return View();
+        }
+        public ActionResult News_single()
+        {
+            return View();
+        }
+    }
+}
